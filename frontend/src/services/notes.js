2f5987b338a901f23764f9152ffaf3060d1ec73e
Samul1 <juutinen.samuli@gmail.com>
2025-06-17 19:28:25 +0300

    uibuild

---
+++ frontend/src/services/notes.js
@@ -0,0 +1,31 @@
+// Moduuli palauttaa nyt olion, jonka kenttinä 
+// (getAll, create ja update) on kolme muistiinpanojen 
+// käsittelyä hoitavaa funktiota. Funktiot palauttavat 
+// suoraan Axiosin metodien palauttaman promisen.
+
+import axios from 'axios'
+const baseUrl = '/api/notes'
+
+const getAll = () => {
+  const request = axios.get(baseUrl)
+  return request.then(response => response.data)
+}
+
+const create = newObject => {
+  const request = axios.post(baseUrl, newObject)
+  return request.then(response => response.data)
+}
+
+const update = (id, newObject) => {
+  const request = axios.put(`${baseUrl}/${id}`, newObject)
+  return request.then(response => response.data)
+}
+
+export default { getAll, create, update }
+/*
+{ 
+  getAll: getAll, 
+  create: create, 
+  update: update 
+}
+*/
