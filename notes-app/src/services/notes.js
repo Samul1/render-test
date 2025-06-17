1d2b7e7ebb9b69c22574bef2e15dfcdbee9e15b0
Samul1 <juutinen.samuli@gmail.com>
2025-06-17 18:26:40 +0300

    eka kommit

---
+++ notes-app/src/services/notes.js
@@ -0,0 +1,36 @@
+// Moduuli palauttaa nyt olion, jonka kenttinä 
+// (getAll, create ja update) on kolme muistiinpanojen 
+// käsittelyä hoitavaa funktiota. Funktiot palauttavat 
+// suoraan Axiosin metodien palauttaman promisen.
+
+import axios from 'axios'
+const baseUrl = 'http://localhost:3001/api/notes'
+
+const getAll = () => {
+  const request = axios.get(baseUrl)
+  const nonExisting = {
+    id: 10000,
+    content: 'This note is not saved to server',
+    important: true,
+  }
+  return request.then(response => response.data.concat(nonExisting))
+}
+
+const create = newObject => {
+  const request = axios.post(baseUrl, newObject)
+  return request.then(response => response.data)
+}
+
+const update = (id, newObject) => {
+  const request = axios.put(`${baseUrl}/${id}`, newObject)
+  return request.then(response => response.data)
+}
+
+export default { getAll, create, update }
+/*
+{ 
+  getAll: getAll, 
+  create: create, 
+  update: update 
+}
+*/
